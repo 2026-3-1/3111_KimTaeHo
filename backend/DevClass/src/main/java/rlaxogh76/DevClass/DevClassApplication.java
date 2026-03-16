ee7900471fff8d4b136f86876a69b237beeb5f98
rlaxogh76 <btm.email2769@gmail.com>
2026-03-16 14:01:15 +0900

    feat: 프로젝트 초기화

---
+++ backend/DevClass/src/main/java/rlaxogh76/DevClass/DevClassApplication.java
@@ -0,0 +1,13 @@
+package rlaxogh76.DevClass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevClassApplication.class, args);
+	}
+
+}
---
+++ backend/DevClass/src/test/java/rlaxogh76/DevClass/DevClassApplicationTests.java
@@ -0,0 +1,13 @@
+package rlaxogh76.DevClass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
