d593c1c6fc905353a8922fa0886312c2e355303d
rlaxogh76 <btm.email2769@gmail.com>
2026-03-30 09:12:20 +0900

    feat: mysql 내보내기

--- Dump20260330/devclass_enrollments.sql
+++ Dump20260330/devclass_enrollments.sql
@@ -38,7 +38,7 @@ CREATE TABLE `enrollments` (
   CONSTRAINT `fk_enrollments_lecture` FOREIGN KEY (`last_watched_lecture_id`) REFERENCES `lectures` (`id`) ON DELETE SET NULL,
   CONSTRAINT `fk_enrollments_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `chk_enrollment_progress` CHECK ((`total_progress` between 0 and 100))
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,7 +47,7 @@ CREATE TABLE `enrollments` (
 
 LOCK TABLES `enrollments` WRITE;
 /*!40000 ALTER TABLE `enrollments` DISABLE KEYS */;
-INSERT INTO `enrollments` VALUES (1,3,1,40,2,'2026-03-23 23:05:32'),(2,3,2,25,7,'2026-03-23 23:05:32'),(3,4,1,100,5,'2026-03-23 23:05:32'),(4,4,3,33,10,'2026-03-23 23:05:32'),(5,3,3,0,NULL,'2026-03-23 23:05:32');
+INSERT INTO `enrollments` VALUES (1,3,1,40,2,'2026-03-23 23:05:32'),(2,3,2,25,7,'2026-03-23 23:05:32'),(3,4,1,100,5,'2026-03-23 23:05:32'),(4,4,3,33,10,'2026-03-23 23:05:32'),(5,3,3,0,NULL,'2026-03-23 23:05:32'),(6,4,27,0,NULL,'2026-03-24 19:33:00'),(7,4,26,0,NULL,'2026-03-24 19:39:40'),(8,4,25,0,NULL,'2026-03-30 09:00:04');
 /*!40000 ALTER TABLE `enrollments` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -60,4 +60,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-03-24  9:05:25
+-- Dump completed on 2026-03-30  9:11:34
